1b5935f226f9fac8530fb214f40cb6af2fba1bf9
Evans, Morgan <evansm54@uksf.kfplc.com>
2021-05-24 15:53:12 +0100

    add cms content

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,5 +1,3 @@
-// https://brm-core-0.brsrvr.com/api/v1/core/?q=drill&rows=10&start=0&request_type=search&search_type=keyword&fl=pid%2Cprice%2Cprice_range%2Csale_price%2Csale_price_range%2Csku_color%2Csku_ids%2Csku_price%2Csku_sale_price%2Csku_swatch_images%2Csku_thumb_images%2Cthumb_image%2Ctitle%2Curl&request_id=71889297486107&account_id=6128&url=screwfix.com
-
 import axios from 'axios';
 
 // components
